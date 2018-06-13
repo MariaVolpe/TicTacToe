c9824935c68e9c918544ec0e74b29963359630b3
Maria Volpe <mariaCHVolpe@gmail.com>
2018-06-13 11:38:41 -0400

    Loser of previous game moves first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+class Square extends Component {
     render() {
+        //squares not clickable if a player has won game
         return (
             <button className="square" onClick={this.props.winner ? null : this.props.handleClick}>
                 {this.props.value}
@@ -12,12 +13,12 @@ class Square extends React.Component {
     }
 }
 
-class Board extends React.Component {
+class Board extends Component {
     constructor(props) {
         super(props);
 
         // intialize squares: blank and not marked
-        // turn - false for player 1, true for player 2
+        // turn - false for player 1 (o), true for player 2 (x)
         // status - text for board
         // winner - if someone has won the game
         this.state = {
@@ -38,15 +39,20 @@ class Board extends React.Component {
         if (this.state.squares[i].marked){
             return;
         }
-        //create new array of squares with one marked x or o and status and update state
+        //create new array of squares with one marked x or o
         const squares = this.state.squares.slice();
-        const status = `Player ${this.turn ? "2" : "1"}'s turn`
         squares[i] = (this.state.turn ? {value: "x", marked: true} : {value: "o", marked: true} );
-
+        //create new status to show who moves next
+        const status = `Player ${this.state.turn ? "1" : "2"}'s turn`;
+        //update state
         this.setState((prevState)=>({squares, turn: !prevState.turn, status}));   
     }
     renderSquare(i) {
-        return <Square turn={this.state.turn} value={this.state.squares[i].value} winner={this.props.winner} handleClick={ () => this.handleClick(i) }/>;
+        return <Square
+            turn={this.state.turn}
+            value={this.state.squares[i].value}
+            winner={this.props.winner}
+            handleClick={ () => this.handleClick(i) }/>;
     }
     // when board updates check for a winner or flag to reset board
     componentDidUpdate(){
@@ -58,10 +64,15 @@ class Board extends React.Component {
                     value: null,
                     marked: false
                     });
-            this.setState(() => ({ squares } ));
+            const turn = turn;
+            //create new status to show who moves next
+            const status = `Player ${this.turn ? "1" : "2"}'s turn`;
+      
+            this.setState(() => ({ status, squares, winner: false } ));
             
             // call reset to unflag board as to be reset
             this.props.reset.call();
+            return;
         }
 
         // check for a winner
@@ -70,7 +81,8 @@ class Board extends React.Component {
         if (winner && !this.state.winner){
             const status = ` Player ${(winner==="o" ? "1" : "2")} wins!`;
             this.props.handleWin(winner);
-            this.setState(() => ({ status, winner: true} ));
+            // loser goes first in next game
+            this.setState(() => ({ status, winner: true, turn: winner === "o" ? true : false }));
             
         }
     }
@@ -99,7 +111,7 @@ class Board extends React.Component {
     }
 }
 
-class Game extends React.Component {
+class Game extends Component {
     constructor(props){
         super(props);
         // player scores
@@ -122,10 +134,9 @@ class Game extends React.Component {
         playerScore[(winner==="o" ? 0 : 1)]++;
         this.setState(()=>({winner: true, playerScore}));
     }
-
     // unflag board as needing to be reset
     setResetFalse(){
-        this.setState(()=>({reset: {value: false, call: this.setResetFalse}}));
+        this.setState(()=>({winner: false, reset: {value: false, call: this.setResetFalse}}));
     }
     render() {
         return (
@@ -147,7 +158,6 @@ class Game extends React.Component {
         );
     }
 }
-
 
 // calculate if there is a winner
 function calculateWinner(squares){
